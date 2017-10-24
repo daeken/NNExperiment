f35bd9919c1e883d57b0e05ea862f17e15a16e58
Cody Brocious <cody.brocious@gmail.com>
2017-10-24 13:20:51 -0400

    Removed old code.

--- nn.py
+++ nn.py
@@ -9,17 +9,6 @@ def sigmoidDerivative(x):
 transfer = sigmoid
 transferDerivative = sigmoidDerivative
 
-# Update network weights with error
-def update_weights(network, row, l_rate):
-	for i in range(len(network)):
-		inputs = row[:-1]
-		if i != 0:
-			inputs = [neuron['output'] for neuron in network[i - 1]]
-		for neuron in network[i]:
-			for j in range(len(inputs)):
-				neuron['weights'][j] += l_rate * neuron['delta'] * inputs[j]
-			neuron['weights'][-1] += l_rate * neuron['delta']
-
 class Layer(object):
 	def __init__(self, count, input, bstore):
 		self.count = count
